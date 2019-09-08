800b695c4b064b036d165c9dfbda9ab442a47b61
Braxton Toomer <braxton.toomer@gmail.com>
2019-09-08 16:49:06 -0700

    Adds index controller action and validations to model Schema, and updates index.ejs to display dates correctly

--- models/flight.js
+++ models/flight.js
@@ -10,19 +10,21 @@ const flightSchema = new Schema({
     type: Number,
     required: true,
     min: 10,
-    max: 9999
+    max: 9999,
+    default: 5555
   },
   departs: {
     type: Date,
-    //if empty string - may need to refactor
-    default: new Date(new Date().setFullYear(new Date().getFullYear() +1))
+    required: true,
+    default: new Date(new Date().setFullYear(new Date().getFullYear() +1))  
+  },
+  time: {
+    type: String,
+    required: true
   }
 }, {
-  timestaps: true
+  timestamps: true
 });
 
-// Compile Schema into a Model and Export
-// First Argument is name of model
-// Second Argument is name of Schema you are compiling into Model
-
 module.exports = mongoose.model('Flight', flightSchema);
+
